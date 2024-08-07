8b6682ea1325b49067d6167a948604a57459d2d4
Denise Salazar <denise.salazar.1210@gmail.com>
2024-08-06 22:32:40 -0700

    reorganize grid and user to their own classes

--- cli.py
+++ cli.py
@@ -2,66 +2,25 @@
 This is a Command Line Interface game of Rock Paper Scissors
 for a human user versus a computer.
 """
-import random
 import pyinputplus as pyip
+from user import User, Computer, UserSymbol
+from grid import Grid
 
-
-class User:
+class GameDriver:
     """
-    A class to represent a user, person or computer.
-
-    Attributes:
-    -----------
-    name : str
-        the username of the user
-    symbol : str
-        the symbol for the user in the game, typically X or O
-    """
-
-    def __init__(self, name, symbol):
-        self.name = name
-        self.symbol = symbol
-
-class Computer(User):
-    """
-    A subclass of User which represents a Computer.
-
-    Attributes:
-    -----------
-    level : int
-        level of computer user
-        1 corresponds to computer choosing x, y coordinates randomly
-    """
-
-    def __init__(self, level):
-        User.__init__(self, "Computer", "O")
-        self.level = 1
-
-class Game:
-    """
-    Game represents a tic, tac, toe board
+    GameDriver represents a game driver for tic, tac, toe.
 
     Attributes:
     -----------
     user_1 : User
     user_2: User
-    grid : String[][]
+    grid : String[][] of ["X", "O", " "]
     """
 
-    NUM_ROWS = 3
-    NUM_COLS = 3
-
-    def show_grid(self):
-        print("Here is the current grid.")
-        for row in self.grid:
-            print(row)
-        print()
-
-
     def __init__(self, user_1, computer_level=1):
-        self.user_1 = User(user_1, "X")
+        self.user_1 = User(user_1, UserSymbol.X.value)
         self.user_2 = Computer(computer_level)
-        self.grid = [[" " for _ in range(self.NUM_COLS)] for _ in range(self.NUM_ROWS)]
+        self.grid = Grid()
 
 
     def play(self):
@@ -69,93 +28,35 @@ class Game:
         user_one_turn = True
         game_over = "It's a Tie!"
 
-        while self._grid_is_open():
+        while self.grid.is_open():
             # show grid
-            self.show_grid()
+            self.grid.pretty_print()
             # choose user
             curr_user = self.user_1
             if not user_one_turn:
                 curr_user = self.user_2
             # play user
-            self._play_user(curr_user)
+            curr_user.play_user(self.grid)
             # check if user won
-            if self._did_user_win(curr_user):
+            if self.grid.did_user_win(curr_user):
                 game_over = "Wahoo! User {} won!".format(curr_user.name)
                 break
             # swtich user
             user_one_turn = not user_one_turn
 
         print(game_over)
-        self.show_grid()
+        self.grid.pretty_print()
 
 
-    def _play_user(self, user):
-        print("Current user is {}!".format(user.name))
-        x, y = -1, -1
-
-        if isinstance(user, Computer):
-            while not self._is_valid_entry(x, y):
-                x = random.randint(0, 2)
-                y = random.randint(0, 2)
-        else:  # type is a User, but not a Computer
-            while not self._is_valid_entry(x, y):
-                x = pyip.inputInt(prompt="Enter valid x coordinate (0, 1, 2): ",
-                                 default=0, min=0, max=2, timeout=180)
-                y = pyip.inputInt(prompt="Enter valid y coordinate (0, 1, 2): ",
-                                 default=0, min=0, max=2, timeout=180)
-        print("You have chosen ({}, {}).".format(x, y))
-        self.grid[x][y] = user.symbol
-
-
-    def _is_valid_entry(self, x, y):
-        return 0 <= x <= 2 and 0 <= y <= 2 and self.grid[x][y] == " "
-
-
-    def _did_user_win(self, user):
-        # check rows
-        for row in self.grid:
-            count = 0
-            for element in row:
-                if element == user.symbol:
-                    count += 1
-            if count == 3:
-                return True
-        # check columns
-        for j in range(0, 3):
-            count = 0
-            for i in range(0, 3):
-                if self.grid[i][j] == user.symbol:
-                    count += 1
-            if count == 3:
-                return True
-        # check left to right diagonal
-        if self.grid[0][0] == self.grid[1][1] == self.grid[2][2] == user.symbol:
-            return True
-        # check right to left diagonal
-        if self.grid[0][2] == self.grid[1][1] == self.grid[2][0] == user.symbol:
-            return True
-        return False
-
-
-    def _grid_is_open(self):
-        for i in range(0, 3):
-            for j in range(0, 3):
-                if self.grid[i][j] == " ":
-                    return True
-        return False
-
-
-def set_up():
-    # username = pyip.inputStr(prompt="Enter username: ",
-    #                          default="user x", blank=False, limit=3)
-    # computer_level = pyip.inputInt(prompt="Enter computer level (1, 2): ",
-    #                          default=1, min=1, max=2, limit=3)
-
-    username = "Denise"
-    computer_level = 1
-    game = Game(username, computer_level)
-    game.play()
+def set_up_game():
+    username = pyip.inputStr(prompt="Enter username: ",
+                             default="user x", blank=False, limit=3)
+    computer_level = pyip.inputInt(prompt="Enter computer level (1, 2): ",
+                             default=1, min=1, max=2, limit=3)
+    game = GameDriver(username, computer_level)
+    return game
 
 
 if __name__ == "__main__":
-    set_up()
+    game = set_up_game()
+    game.play()
---
+++ user.py
@@ -0,0 +1,87 @@
+from enum import Enum
+import random
+import pyinputplus as pyip
+
+CURRENT_USER_PROMPT = "Current user is {}!"
+COORDINATES_CHOSEN_PROMPT = "You have chosen ({}, {})."
+
+
+class UserSymbol(Enum):
+    X = "X"
+    O = "O"
+    Blank = " "
+
+class User:
+    """
+    A class to represent a user - person or computer.
+
+    Attributes:
+    -----------
+    name : str
+        the username of the user
+    symbol : str
+        the symbol for the user in the game, typically X or O
+    """
+
+    ENTER_X_PROMPT = "Enter valid x coordinate (0, 1, 2): "
+    ENTER_Y_PROMPT = "Enter valid y coordinate (0, 1, 2): "
+
+    def __init__(self, name, symbol):
+        self.name = name
+        self.symbol = symbol
+
+    def play_user(self, grid):
+        print(CURRENT_USER_PROMPT.format(self.name))
+        x, y = -1, -1
+        while not grid.is_valid_entry(x, y):
+            x = pyip.inputInt(prompt=self.ENTER_X_PROMPT,
+                             default=0, timeout=180,
+                             min=grid.MIN_COORDINATE, 
+                             max=grid.MAX_COORDINATE)
+            y = pyip.inputInt(prompt=self.ENTER_Y_PROMPT,
+                             default=0, timeout=180,
+                             min=grid.MIN_COORDINATE, 
+                             max=grid.MAX_COORDINATE)        
+            print(COORDINATES_CHOSEN_PROMPT.format(x, y))
+        grid.grid[x][y] = self.symbol
+
+
+class Computer(User):
+    """
+    A subclass of User which represents a Computer.
+
+    Attributes:
+    -----------
+    level : int
+        level of computer user
+        1 corresponds to computer choosing first opening found
+        2 corresponds to computer choosing x, y coordinates randomly
+        3 corresponds to computer blocks other users win, if found
+    """
+
+    COMPUTER_NAME = "Computer"
+    COMPUTER_SYMBOL = UserSymbol.O.value
+
+    def __init__(self, level):
+        User.__init__(self, self.COMPUTER_NAME, self.COMPUTER_SYMBOL)
+        self.level = level
+
+    def play_user(self, grid):
+        print(CURRENT_USER_PROMPT.format(self.name))
+        x, y = -1, -1
+        # Level 1: choose 1st opening found
+        if self.level == 1:
+            for i in range(0, grid.NUM_ROWS):
+                for j in range(0, grid.NUM_COLS):
+                    if grid.grid[i][j] == " ":
+                        x, y = i, j
+                        break
+        # Level 2: choose coordinates randomnly
+        elif self.level == 2:
+            while not grid.is_valid_entry(x, y):
+                x = random.randint(grid.MIN_COORDINATE, grid.MAX_COORDINATE)
+                y = random.randint(grid.MIN_COORDINATE, grid.MAX_COORDINATE)
+        # TODO: Level 3: choose a spot that other user is about to win
+
+        print(COORDINATES_CHOSEN_PROMPT.format(x, y))
+        grid.grid[x][y] = self.symbol
